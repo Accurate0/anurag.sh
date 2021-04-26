1b037b3364bae21bf7a95953727d1041648c2d6b
Anurag <anuragsingh3@outlook.com>
2021-04-26 23:09:00 +0800

    scale font size

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container min-w-full">
     <div class="flex gap-8 flex-col items-center justify-center h-screen">
-      <div class="text-7xl subpixel-antialiased select-none">Anurag Singh</div>
+      <div class="text-4xl lg:text-7xl subpixel-antialiased select-none">
+        Anurag Singh
+      </div>
     </div>
   </div>
 </template>
