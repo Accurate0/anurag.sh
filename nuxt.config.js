cff395b96d9d87f398f77c7454e181671deedae9
Anurag <anuragsingh3@outlook.com>
2021-04-26 22:04:39 +0800

    very important website

---
+++ nuxt.config.js
@@ -0,0 +1,32 @@
+export default {
+  ssr: false,
+  target: 'static',
+  head: {
+    title: 'anurag.sh',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      {
+        ref: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap',
+      },
+    ],
+  },
+
+  css: ['~/assets/style'],
+  plugins: [],
+  components: true,
+  buildModules: ['@nuxt/typescript-build', 'nuxt-windicss'],
+
+  modules: [],
+  build: {},
+}
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container min-w-full">
+    <div class="flex gap-8 flex-col items-center justify-center h-screen">
+      <div class="text-7xl subpixel-antialiased select-none">Anurag Singh</div>
+    </div>
+  </div>
+</template>
