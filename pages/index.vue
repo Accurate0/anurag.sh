3d4bcfb8a219192b219d04d964d30a2ee84b62db
Anurag <anuragsingh3@outlook.com>
2021-04-27 14:03:20 +0800

    add linkedin + update theme

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
   <div
     class="lowercase subpixel-antialiased flex gap-6 flex-col items-center justify-center h-screen"
   >
-    <div class="capitalize text-4xl md:text-7xl">Anurag Singh</div>
-    <div class="text-xs md:text-lg">
+    <div class="capitalize text-5xl md:text-7xl">Anurag Singh</div>
+    <div class="md:text-lg hidden md:block">
       software engineer <b class="lg:text-2xl">∣</b> 6<sup>th</sup> Place at
       <a
         href="https://capture.tf/"
@@ -14,22 +14,27 @@
       day)
     </div>
     <div
-      class="select-none flex text-center flex-col md:gap-3 gap-6 text-4xl md:text-1xl"
+      class="select-none flex text-center flex-col gap-3 text-2xl md:text-1xl"
     >
       <div>
-        <a @click="specialToggle()">theme</a>
+        <nuxt-link to="/blog">blog</nuxt-link>
       </div>
       <div>
         <nuxt-link to="/projects">projects</nuxt-link>
       </div>
-      <div>
-        <nuxt-link to="/blog">blog</nuxt-link>
-      </div>
+      <br />
       <div>
         <a href="mailto:contact@anurag.sh">email</a>
       </div>
       <div>
         <a href="https://github.com/Accurate0">github</a>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/anurag-singh8/">linkedin</a>
+      </div>
+      <br />
+      <div>
+        <a @click="specialToggle()">theme</a>
       </div>
     </div>
   </div>
