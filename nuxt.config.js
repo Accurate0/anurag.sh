3d927acf5635a92570e217cae63849236878eff4
Anurag <contact@anurag.sh>
2021-07-11 20:37:00 +0800

    Update title to match

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { property: 'og:title', content: 'anurag singh' },
+      { property: 'og:title', content: 'Anurag Singh' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
   },
