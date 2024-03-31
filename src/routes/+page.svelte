5d1066ae1a40ea34a3f5db7975d20868f6da3d68
Anurag Singh <contact@anurag.sh>
2024-03-31 16:25:52 +0800

    ego

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,11 @@
 <div>
-	<h2 class="text-4xl pb-4">anurag singh</h2>
+	<h2 class="text-4xl pb-4">anurag</h2>
 	<div class="grid gap-2 grid-flow-col">
-		<div>
-			<a href="https://assets.anurag.sh/resume.pdf">resume</a>
-		</div>
-		<div>|</div>
 		<div>
 			<a href="mailto:hey@anurag.sh">email</a>
 		</div>
-		<div>|</div>
 		<div>
 			<a href="https://github.com/Accurate0">github</a>
-		</div>
-		<div>|</div>
-		<div>
-			<a href="https://www.linkedin.com/in/anurag-singh8/">linkedin</a>
 		</div>
 	</div>
 </div>
